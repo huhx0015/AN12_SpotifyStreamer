9f48760916559c46287e4fff4cdb8b9cc3804dc6
Michael Y. Huh <mhuh@yoonhuh.com>
2015-06-11 00:11:34 -0700

    6/11/2015 (12:11 AM PST): * Initial commit.

---
+++ app/src/main/java/com/huhx0015/spotifystreamer/SSMainActivity.java
@@ -0,0 +1,43 @@
+package com.huhx0015.spotifystreamer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import huhx0015.com.spotifystreamer.R;
+
+public class SSMainActivity extends AppCompatActivity {
+
+    /** ACTIVITY LIFECYCLE METHODS _____________________________________________________________ **/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ss_main_activity);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.ss_main_activity_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /** LAYOUT METHODS _________________________________________________________________________ **/
+}
